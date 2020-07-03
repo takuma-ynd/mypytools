f25cd5599510b034c72864072deb0015df28af72
Takuma Yoneda <takuma-ynd@users.noreply.github.com>
2020-07-03 11:42:58 +0900

    fix __init__.py

--- __init__.py
+++ __init__.py
@@ -1,2 +1,3 @@
 from mypytools import misc
 from mypytools.prepare_output_dir import prepare_output_dir
+from mypytools.timed import timed
--- misc/__init__.py
+++ misc/__init__.py
@@ -1,1 +1,5 @@
 from mypytools.misc.is_return_code_zero import is_return_code_zero
+from mypytools.misc.print_color import print_green
+from mypytools.misc.print_color import print_blue
+from mypytools.misc.print_color import print_yellow
+from mypytools.misc.print_color import print_red
